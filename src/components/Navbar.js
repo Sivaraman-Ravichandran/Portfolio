6d7dc13507b3a7ffa812af066c5eddd38ef104ba
Sivaraman-Ravichandran <sivcloud.12@gmail.com>
2024-11-18 22:22:15 +0530

    removed some unused imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,6 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { TbBrandLeetcode } from "react-icons/tb";
 import { SiLeetcode, SiGeeksforgeeks, SiGithub } from "react-icons/si";
 import {
   AiFillStar,
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -22,7 +16,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath=""
               isBlog={false}
               title="FindDoc"
               description="A platform connects patients with top-rated physicians, allowing them to easily search, compare, and book appointments. It leverages advanced algorithms and user data to provide personalized recommendations and streamline the healthcare experience. Tech Stack: React, Springboot, and MongoDB."
@@ -32,7 +26,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath=""
               isBlog={false}
               title="Shield Android"
               description="A mobile application that enchance the women safety by navigating them into the safest location with police controlling in the dashboard. Tech Stack : React Native, flask, MongoDB."
@@ -42,7 +36,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath=""
               isBlog={false}
               title="Shield Police Dashboard"
               description="A Web Based application that monitors the activities from the mobile application shield to enhance women (safety) which is mentioned last. And also this application monitors the public CCTV for threats and risk with real time integration like gender classification, Gesture Recognition.Tech Stack: React, Flask, MediaPipe(gesture recognition), Mapbox(map integration), MongoDB."
@@ -51,7 +45,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath=""
               isBlog={false}
               title="Bharath College"
               description="Portfolio-site for bharath medical college. build this web application for the client during internship."
@@ -62,7 +56,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath=""
               isBlog={false}
               title="Simon Game"
               description="Try this and immerse yourself in the nostalgic delight of the Simon Game, where memory meets melody. Follow the mesmerizing sequence of vibrant lights and tones, testing your recall skills with each level. Tech Stack: HTML, CSS, JS, JQuery"
@@ -70,8 +64,6 @@ function Projects() {
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
-
-      
         </Row>
       </Container>
     </Container>
