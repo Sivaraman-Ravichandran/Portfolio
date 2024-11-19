fdebc2c466b792a6ce96f7c31c387c1da80afaac
Sivaraman-Ravichandran <sivcloud.12@gmail.com>
2024-11-19 11:19:15 +0530

    again removed some unudes components

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
-
 function AboutCard() {
   return (
     <Card className="quote-card-view">
@@ -11,13 +9,15 @@ function AboutCard() {
             Hi Everyone, I am <span className="purple">Sivaraman </span>
             from <span className="purple"> Coimbatore, India.</span>
             <br />
-            I am pursuing Bachelor's Degree in Computer Science and Engineering specialization with Cyber Security.
+            I am pursuing Bachelor's Degree in Computer Science and Engineering
+            specialization with Cyber Security.
             <br />
-            <br/>
+            <br />
           </p>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "A genius is the man who can do the average thing when everyone else around him is login his mind."{" "}
+            "A genius is the man who can do the average thing when everyone else
+            around him is login his mind."{" "}
           </p>
           <footer className="blockquote-footer">Sherlock Holmes</footer>
         </blockquote>
